90d1c89ef533ccc0f830a0577795d64da34e0485
vikas164u <mishra.vikas16@gmail.com>
2017-01-12 14:19:07 +0100

    k new files

---
+++ src/main/java/com/test/java/MyAppClass.java
@@ -0,0 +1,31 @@
+/*
+ * @author : Vikas Mishra 
+ * @Date   : Jan 12, 2017
+ * @Project: Bliffoscope DataAnalysis 
+ * @Client : SNOW
+ * 
+ */
+/*
+ * @author : Vikas Mishra 
+ * @Date   : Jan 12, 2017
+ * @Project: Bliffoscope DataAnalysis 
+ * @Client : SNOW
+ * 
+ */
+package com.test.java;
+
+/**
+ * @author SYNEAMS01
+ *
+ */
+public class MyAppClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
