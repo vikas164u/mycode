eddee2be3907d88499d96c34168b51e0e461ceeb
vikas164u <mishra.vikas16@gmail.com>
2017-01-12 12:28:50 +0100

    First Commit from Eclipse

---
+++ src/main/java/javagit/javaprj/git/App.java
@@ -0,0 +1,13 @@
+package javagit.javaprj.git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
